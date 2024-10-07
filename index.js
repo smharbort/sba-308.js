a739163295915f6821e8527d85f17ca5b3312447
smh <smharbort@outlook.com>
2024-10-07 09:20:19 -0400

    initializing

---
+++ index.js
@@ -0,0 +1,24 @@
+// input -> courseInfo -> object -> key name/"data-type expected"
+/* {
+    id: "number",
+    name: "string",
+    courseId : "number",
+    group_weight: "number",
+    assignments: [assignmentInfo]
+
+} */
+
+// input -> assignmentGroup
+
+
+
+// input -> assignmentInfo
+
+
+// input -> learnerSubmission
+
+
+// function getLearnerData(courseInfo, assignmentGroup, [learnerSubmission])
+
+
+// OUTPUT -> formatted result, should be an array of objects
